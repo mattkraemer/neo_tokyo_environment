d79c7f0a1f20373c91938f9c856c8d75db289e15
malmosands <matthias@c1x.dev>
2021-05-16 17:58:03 +0200

    update

--- app/index.js
+++ app/index.js
@@ -13,19 +13,24 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+const camera = new THREE.PerspectiveCamera(17, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0,0,25);
 
 // Renderer
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+// Lights
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.15 );
+scene.add( directionalLight );
 
 // 3D Model
 const loader = new GLTFLoader();
 
 loader.load('../assets/neo_tokyo/scene.gltf', function(gltf) {
-  scene.scale.set(0.01,0.01,0.01);
+  scene.scale.set(0.1,0.1,0.1);
+  scene.rotation.x += .75;
   scene.add(gltf.scene);
 }, undefined, function(error) {
   console.error(error);
@@ -34,6 +39,8 @@ loader.load('../assets/neo_tokyo/scene.gltf', function(gltf) {
 // Animate
 function animate() {
   requestAnimationFrame(animate);
+
+  scene.rotation.y += 0.0005;
 
   renderer.render(scene,camera);
 }
